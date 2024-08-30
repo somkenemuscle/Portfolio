1f1aaae9fcc489c4ba50e59c8d69d9a540c411f2
Somkene Ojukwu <somkeneoj@gmail.com>
2024-08-30 14:24:27 +0100

    5: footer implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Navbar from "@/components/shared/Navbar";
 import HeroSection from "@/components/shared/heroSection";
-// import { About } from "@/components/shared/About";
 import Projects from "@/components/shared/Projects";
 import Skills from "@/components/shared/Skills";
 import Contact from "@/components/shared/contact";
@@ -12,7 +11,6 @@ export default function Home() {
     <main>
       <Navbar />
       <HeroSection />
-      {/* <About /> */}
       {/* <Projects /> */}
       <Skills />
       <Contact />
