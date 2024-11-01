519b89890e96ee2c048858147cdc58ca91334e50
Somkene Ojukwu <somkeneoj@gmail.com>
2024-11-01 20:06:17 +0100

    UI Updated

--- constants/navigation.ts
+++ constants/navigation.ts
@@ -3,7 +3,7 @@ export const navigation = [
     { name: 'Projects', href: '#projects' },
     { name: 'Skills', href: '#skills' },
     { name: 'About', href: '#about' },
-    { name: 'Resume', href: '#contact' }
+    { name: 'Resume', href: 'https://utfs.io/f/rXxdhjbFRcsm0jpQhoiJjFlTbaKcgZwWY1xqVXMnQDNdr6tv' }
 ];
 
 
