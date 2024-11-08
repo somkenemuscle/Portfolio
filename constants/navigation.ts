6e640120663889433db9acfc114071e3d3eee84c
Somkene Ojukwu <somkeneoj@gmail.com>
2024-11-08 21:43:43 +0100

    new resume uploaded

--- constants/navigation.ts
+++ constants/navigation.ts
@@ -3,7 +3,7 @@ export const navigation = [
     { name: 'Projects', href: '#projects' },
     { name: 'Skills', href: '#skills' },
     { name: 'About', href: '#about' },
-    { name: 'Resume', href: 'https://utfs.io/f/rXxdhjbFRcsm0jpQhoiJjFlTbaKcgZwWY1xqVXMnQDNdr6tv' }
+    { name: 'Resume', href: 'https://utfs.io/f/rXxdhjbFRcsmqeDfzvLAg19Zkx3TWniNDhroBma8M64FO5Ie' }
 ];
 
 
