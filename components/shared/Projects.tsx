4e7519de885137f88e29ef908c4da873c067f3b3
Somkene Ojukwu <somkeneoj@gmail.com>
2025-01-10 14:43:01 +0100

    Working on seconD uI

--- components/shared/Projects.tsx
+++ components/shared/Projects.tsx
@@ -43,14 +43,14 @@ export const Projects = () => {
 
                             {/* GRID SECTION */}
                             <section className="grid grid-cols-12 gap-4">
-                                <div className={`col-span-12 md:col-span-6 lg:col-span-6 ${project.device === 'laptop' && 'rounded-tr-3xl border-t-[3px] border-r-[3px] border-zinc-800'}`}>
+                                <div className={`col-span-12 md:col-span-6 lg:col-span-6 ${project.device === 'laptop' && ' md:rounded-tr-3xl md:border-t-[3px] md:border-r-[3px] md:border-zinc-800'}`}>
                                     <img
                                         src={project.image}
                                         alt={`${project.title} project screenshot`}
-                                        className={`object-cover h-[400px] mx-auto ${project.device === 'laptop' && 'object-left border-t-[16px]  border-r-[16px] border-neutral-950 rounded-tr-3xl rounded-bl-3xl'} `}
+                                        className={`object-cover h-[400px] mx-auto ${project.device === 'laptop' && 'sm:w-full sm:object-center md:object-left md:border-t-[16px] md:border-r-[16px] md:border-neutral-950 md:rounded-tr-3xl md:rounded-bl-3xl'} `}
                                     />
                                 </div>
-                                <div className="col-span-12 md:col-span-6 lg:col-span-6 pl-4 pr-8 pt-11 text-left">
+                                <div className="col-span-12 md:col-span-6 lg:col-span-6 pl-6 md:pl-4 pr-8 pt-11 text-left">
                                     {/* HEADER SECTION */}
                                     <h1 className="text-gray-200 text-3xl mb-1 font-medium flex items-center gap-2">
                                         <project.icon className="bg-neutral-950 p-2 rounded-full h-10 w-10 border border-neutral-400" /> {project.title}
@@ -58,16 +58,16 @@ export const Projects = () => {
                                     <p className="text-gray-400 tracking-wide pt-2 font-light">{project.description}</p>
 
                                     {/* TECHNOLOGIES USED ICONS */}
-                                    <ul className="pt-11 flex items-center">
+                                    <ul className="pt-11 flex items-center gap-2">
                                         {project.technologies.map((tech, techIndex) => (
                                             <li key={techIndex}>
-                                                <img className={`inline-block mr-4 w-7 h-7 ${tech.style || ''}`} src={tech.icon} alt={`${tech.name} logo`} />
+                                                <img className={`mr-4 w-7 h-7 ${tech.style || ''}`} src={tech.icon} alt={`${tech.name} logo`} />
                                             </li>
                                         ))}
                                     </ul>
 
                                     {/* LIVE PREVIEW LINK */}
-                                    <section className="pt-11 w-fit">
+                                    <section className="pt-11">
                                         <Link href={project.livePreview} target="_blank" rel="noopener noreferrer">
                                             <p className="text-gray-300 font-medium tracking-wider rounded-xl text-sm hover:underline">
                                                 🌍 Live Preview →
