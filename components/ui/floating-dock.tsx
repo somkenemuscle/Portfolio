666b47bae42e9fa4f2c36cf1cbff0f5e173c1105
Somkene Ojukwu <somkeneoj@gmail.com>
2025-01-11 05:02:13 +0100

    Animations added

--- components/ui/floating-dock.tsx
+++ components/ui/floating-dock.tsx
@@ -70,6 +70,7 @@ const FloatingDockMobile = ({
                 <Link
                   href={item.href}
                   key={item.title}
+                  target="_blank" rel="noopener noreferrer"
                   className="h-10 w-10 rounded-full bg-neutral-900 dark:bg-neutral-900 flex items-center justify-center"
                 >
                   <div className="h-4 w-4">{item.icon}</div>
@@ -167,7 +168,7 @@ function IconContainer({
   const [hovered, setHovered] = useState(false);
 
   return (
-    <Link href={href}>
+    <Link href={href} target="_blank" rel="noopener noreferrer">
       <motion.div
         ref={ref}
         style={{ width, height }}
