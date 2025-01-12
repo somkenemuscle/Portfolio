4c8bd1a573ce4b22c6ca1dc478d0ff832c2ff769
Somkene Ojukwu <somkeneoj@gmail.com>
2025-01-12 01:33:12 +0100

    Animations added

--- constants/navigation.ts
+++ constants/navigation.ts
@@ -2,7 +2,7 @@
 export const navigationOnLargeScreen = [
     { name: 'Projects', href: '#projects' },
     { name: 'About', href: '#' },
-    { name: 'Resume', href: 'https://ipw88zuno5.ufs.sh/f/rXxdhjbFRcsmx7XlrNf0ifsPVWXJyF6KZuHGQ1gpcqtBOA7k' },
+    { name: 'Resume', href: 'https://ipw88zuno5.ufs.sh/f/rXxdhjbFRcsmckYLL49C3e57ruqTbIwUxhVagn8QYRjs2NLF' },
     { name: 'Contact', href: '#contact' },
 ];
 
@@ -10,7 +10,7 @@ export const navigationOnLargeScreen = [
 
 export const navigationOnMobile = [
     { name: 'Projects', href: '#projects' },
-    { name: 'Resume', href: 'https://ipw88zuno5.ufs.sh/f/rXxdhjbFRcsmx7XlrNf0ifsPVWXJyF6KZuHGQ1gpcqtBOA7k' },
+    { name: 'Resume', href: 'https://ipw88zuno5.ufs.sh/f/rXxdhjbFRcsmckYLL49C3e57ruqTbIwUxhVagn8QYRjs2NLF' },
     { name: 'Contact', href: '#contact' }
 ];
 
