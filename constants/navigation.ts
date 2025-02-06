3595039bb11a3351ea7bdf7d6e5b924d4769705e
Somkene Ojukwu <somkeneoj@gmail.com>
2025-02-06 22:20:22 +0100

    Resumee Updated

--- constants/navigation.ts
+++ constants/navigation.ts
@@ -2,7 +2,7 @@
 export const navigationOnLargeScreen = [
     { name: 'Projects', href: '#projects' },
     { name: 'About', href: '#' },
-    { name: 'Resume', href: 'https://ipw88zuno5.ufs.sh/f/rXxdhjbFRcsmiajmCOSMChHUEfNL2PcVp9tKsx713drmIZBb' },
+    { name: 'Resume', href: 'https://ipw88zuno5.ufs.sh/f/rXxdhjbFRcsmBUFrZPfAUhgmKYwtQzGVMp1x37cAZOrsqafb' },
     { name: 'Contact', href: '#contact' },
 ];
 
@@ -10,7 +10,7 @@ export const navigationOnLargeScreen = [
 
 export const navigationOnMobile = [
     { name: 'Projects', href: '#projects' },
-    { name: 'Resume', href: 'https://ipw88zuno5.ufs.sh/f/rXxdhjbFRcsmiajmCOSMChHUEfNL2PcVp9tKsx713drmIZBb' },
+    { name: 'Resume', href: 'https://ipw88zuno5.ufs.sh/f/rXxdhjbFRcsmBUFrZPfAUhgmKYwtQzGVMp1x37cAZOrsqafb' },
     { name: 'Contact', href: '#contact' }
 ];
 
